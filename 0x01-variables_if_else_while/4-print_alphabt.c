52493095db7443f3590ac4f47dc88494f87ad406
RealLaone <lkkwati@gmail.com>
2023-06-21 16:41:23 -0700

    Program to print lowercase alphabets except 2

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
  * main - Entry point
- * Discription: 'Print lowercase alphabets except some'
+ * Description: 'Print lowercase alphabets except some'
  * Return: Always 0
 */
 int main(void)
