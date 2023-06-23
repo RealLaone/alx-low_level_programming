56be671a1d40a2784c99d22f0b96f7f77166a28c
RealLaone <lkkwati@gmail.com>
2023-06-22 17:35:40 -0700

    Function that prints the 9x table starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
-* main - Entry point
-* Description: 'times_table - prints the 9 times table, starting with 0'
+* times_table - prints the 9 times table, starting with 0
 */
 void times_table(void)
 {
