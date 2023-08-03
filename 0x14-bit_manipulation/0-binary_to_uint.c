cc76289b228287f0c958f7f22c5c247b4c32cdfb
RealLaone <lkkwati@gmail.com>
2023-08-03 13:47:13 -0700

    function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,26 +8,25 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int dec_val = 0;
-    unsigned int bit_value = 1;
-    int i;
+	unsigned int dec_val = 0;
+	unsigned int bit_value = 1;
+	int i;
 
-    if (!b)
-        return (0);
+	if (!b)
+		return (0);
 
-    for (i = 0; b[i]; i++)
-    {
-        if (b[i] != '0' && b[i] != '1')
-            return (0);
 
-        bit_value = 1;
-        for (int j = 0; j < (i - 1); j++) 
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 
-	/* Calculate the power of 2 using a loop*/
-            bit_value *= 2;
+		bit_value = 1;
+		for (int j = 0; j < (i - 1); j++)
+		bit_value *= 2;
 
-        dec_val += (b[i] - '0') * bit_value;
-    }
+		dec_val += (b[i] - '0') * bit_value;
+	}
 
-    return (dec_val);
+	return (dec_val);
 }
