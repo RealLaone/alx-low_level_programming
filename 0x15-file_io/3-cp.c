445b2b8ac902713ecfbb016d361c3225fafd921d
RealLaone <lkkwati@gmail.com>
2023-08-07 10:53:21 -0700

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,6 @@ char *create_buffer(char *file)
 
 	if (buffer == NULL)
 	{
-		/* Print an error message to stderr and exit with code 99 */
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
@@ -36,7 +35,6 @@ void close_file(int fd)
 
 	if (c == -1)
 	{
-		/* Print an error message to stderr and exit with code 100 */
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
@@ -59,44 +57,34 @@ int main(int argc, char *argv[])
 	int from, to, r, w;
 	char *buffer;
 
-	/* Check if the number of arguments is correct */
 	if (argc != 3)
 	{
-		/* Print an error message to stderr and exit with code 97 */
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
 	buffer = create_buffer(argv[2]); /* Allocate buffer */
 	from = open(argv[1], O_RDONLY); /* Open file_from in read-only mode */
-	r = read(from, buffer, 1024); /* Read data into the buffer */
-	/* Open file_to with specific flags */
+	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		/* Check for read errors */
 		if (from == -1 || r == -1)
 		{
-			/* Print error message to stderr, free buffer, & exit with code 98 */
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
 
-		/* Write data from the buffer to file_to */
 		w = write(to, buffer, r);
-		/* Check for write errors */
 		if (to == -1 || w == -1)
 		{
-			/* Print an error message to stderr, free buffer, and exit with code 99 */
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
 
-		/* Read more data into the buffer */
 		r = read(from, buffer, 1024);
-		/* Open file_to in append mode */
 		to = open(argv[2], O_WRONLY | O_APPEND);
 	}
 
@@ -106,5 +94,5 @@ int main(int argc, char *argv[])
 	close_file(from); /* Close file_from */
 	close_file(to); /* Close file_to */
 
-	return (0); /* Return 0 to indicate successful execution */
+	return (0);
 }
